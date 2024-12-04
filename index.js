0ede7417fb18b877a388dc72210aab229e90ad81
caleb <musyokacaleb5@gmail.com>
2024-12-04 08:45:40 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// code your solution here
+// Define the saturdayFun function
+ function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // Define the mondayWork function
+  const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  };
+  
+  // Define the wrapAdjective function
+  function wrapAdjective(highlight = "*") {
+    return function(adjective) {
+      return `You are ${highlight}${adjective}${highlight}!`;
+    };
+  }
+  
+  // Export the functions for testing
+  module.exports = {
+    saturdayFun,
+    mondayWork,
+    wrapAdjective
+  };
+  
